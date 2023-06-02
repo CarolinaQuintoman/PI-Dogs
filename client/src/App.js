410b2a8d6ea4b682aae8b6ccaccfb8ce581e0b9a
CarolinaQuintoman <quintoman.carolina@gmail.com>
2023-06-02 19:42:46 -0300

    app.js NF

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import NavBar from './components/NavBar/NavBar';
 import { Landing, Home, Form, Detail, NotFound } from './views';
-import { Route, useLocation, Navigate } from 'react-router-dom';
+import { Route, useLocation, Navigate, BrowserRouter as Router, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 axios.defaults.baseURL = "https://pi-dogs-production-2b3c.up.railway.app/";
 
 function App() {
-  const location = useLocation()
+  const location = useLocation();
   return (
-    <>
-      {location.pathname !== "/" && <NavBar/>}
-        <Route exact path="/" component={Landing}/> 
-        <Route path="/home" render={() => <Home />}/>  
-        <Route path="/detail/:id" render={() => <Detail /> }/>
-        <Route path="/create" render={() => <Form /> }/> 
-        <Route path="/404" component={ <NotFound />}/>
-        <Route path="*" component={<Navigate to="/404" />}/>
-    </>
+    <Router>
+      {location.pathname !== "/" && <NavBar />}
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/home" component={Home} />
+        <Route path="/detail/:id" component={Detail} />
+        <Route path="/create" component={Form} />
+        <Route path="/404" component={NotFound} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Switch>
+    </Router>
   );
 }
 
