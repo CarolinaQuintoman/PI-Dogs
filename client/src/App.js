5f3cb2956ec2f98f1a9f0e6b3c3e53082ed19dab
CarolinaQuintoman <quintoman.carolina@gmail.com>
2023-06-02 19:49:04 -0300

    arreglando NF 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import NavBar from './components/NavBar/NavBar';
 import { Landing, Home, Form, Detail, NotFound } from './views';
-import { Route, useLocation, Navigate, BrowserRouter as Router, Switch } from 'react-router-dom';
-import axios from 'axios';
-
-axios.defaults.baseURL = "https://pi-dogs-production-2b3c.up.railway.app/";
+import { Route, Switch, useLocation, Redirect } from 'react-router-dom';
 
 function App() {
   const location = useLocation();
   return (
-    <Router>
-      {location.pathname !== "/" && <NavBar />}
+    <>
+      {location.pathname !== "/" && <NavBar/>}
       <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/home" component={Home} />
-        <Route path="/detail/:id" component={Detail} />
-        <Route path="/create" component={Form} />
-        <Route path="/404" component={NotFound} />
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Switch>
-    </Router>
+        <Route exact path="/" component={Landing}/> 
+        <Route path="/home" render={() => <Home />}/>  
+        <Route path="/detail/:id" render={() => <Detail /> }/>
+        <Route path="/create" render={() => <Form /> }/> 
+        <Redirect from="/old-route" to="/new-route" /> {/* Redireccionar rutas antiguas */}
+        <Route component={NotFound}/> {/* Ruta para NotFound */}
+      </Switch> 
+    </>
   );
 }
 
